e11fa18712f9b08305b8f7e709ccf59bc6e71bd1
Ted Kim <taehyun.kim@canada.ca>
2021-07-07 20:11:15 -0400

    tabs?

--- zdl.sh
+++ zdl.sh
@@ -6,40 +6,40 @@ set -e
 
 # Print input info
 usage() {
-	echo Downloading Record "$RECORD_ID" to "$DL_DIR"
+    echo Downloading Record "$RECORD_ID" to "$DL_DIR"
       }
 # Report error and exit
 perror() {
-	echo Error: "$1" >&2
-	exit 1
+    echo Error: "$1" >&2
+    exit 1
 }
 
 # Check if all params are supplied
 if [ "$#" -eq 0 ]; then
-	perror "Missing parameters"
-	exit 1
+    perror "Missing parameters"
+    exit 1
 fi
 
 # Parameters
 while [[ "$#" -gt 0 ]]; do
-	case $1 in
-		-i|--id) RECORD_ID="$2";
-			shift;shift;;
-		-d|--dir) DL_DIR="$2";
-			shift;shift;;
-		*) echo Unknown parameter passed: "$1";
-			exit 1;
-	esac;
-	if [[ -z $RECORD_ID ]]
-		then	
-			echo "Error: Record ID must be declared (-i, --id)"
-			exit 1
-	fi
+    case $1 in
+        -i|--id) RECORD_ID="$2";
+            shift;shift;;
+        -d|--dir) DL_DIR="$2";
+            shift;shift;;
+        *) echo Unknown parameter passed: "$1";
+            exit 1;
+    esac;
+    if [[ -z $RECORD_ID ]]
+        then    
+            echo "Error: Record ID must be declared (-i, --id)"
+            exit 1
+    fi
 done
 
 if [[ -z $DL_DIR ]]
-	then
-		DL_DIR=`pwd`; 
+    then
+        DL_DIR=`pwd`; 
 fi
 
 # Check HTTP status
@@ -47,10 +47,10 @@ fi
 HTTP_STATUS="$(curl -ILs https://zenodo.org/record/"$RECORD_ID" | head -1 | sed 's/HTTP\/.* \([0-9]*\) .*/\1/')";
 
 if [[ $HTTP_STATUS != 200 ]]
-	then
-		echo Record cannot be reached;
-		echo Error \(HTTP: "$HTTP_STATUS"\);
-	exit 1
+    then
+        echo Record cannot be reached;
+        echo Error \(HTTP: "$HTTP_STATUS"\);
+    exit 1
 fi
 
 # Main
@@ -59,16 +59,16 @@ FILES="$(curl -X GET https://zenodo.org/record/"$RECORD_ID" | grep -o 'href=\"\/
 FILE_NO="$(echo -n "$FILES" | grep -c '^')";
 
 if [[ $FILE_NO -eq 0 ]]
-	then
-		echo No download links found;
-	exit 1
+    then
+        echo No download links found;
+    exit 1
 fi
 
 echo Found "$FILE_NO" files;
 
 while IFS= read -r line
-	do
-		wget https://zenodo.org/"$line";
+    do
+        wget https://zenodo.org/"$line";
 done <<<$FILES
 
 exit 0
